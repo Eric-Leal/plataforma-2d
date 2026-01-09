a5076fbbe2cd90e5adab6e7d0f401c54086e9700
Eric Leal <ericgleal@gmail.com>
2026-01-09 18:11:39 -0300

    Create level transitions

---
+++ scripts/camera.gd
@@ -0,0 +1,21 @@
+extends Camera2D
+
+var target: Node2D
+
+func _ready() -> void:
+	getTarget()
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	position = target.position
+
+
+func getTarget():
+	var nodes = get_tree().get_nodes_in_group("Player")
+	
+	if nodes.size() == 0:
+		push_error("Player not found")
+		return
+	
+	target = nodes[0]
