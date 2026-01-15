5c626bd8272d1325c54b2355fe4f632db4983593
Eric Leal <ericgleal@gmail.com>
2026-01-15 13:26:14 -0300

    ADD run and slide states

--- scripts/player.gd
+++ scripts/player.gd
@@ -22,10 +22,17 @@ const WALK_SPEED = 60.0
 const DUCK_SPEED = 30.0
 const RUN_SPEED = 110.0
 const JUMP_VELOCITY = -250.0
+const SLIDE_COOLDOWN_FACTOR = 3
+const SLIDE_BONUS_DISTANCE = 35
+var slide_cooldown = 0
+var slide_speed_bonus = 0
+var sliding_speed = current_speed
+var sliding: bool
 var current_speed = WALK_SPEED
 var direction = 0
 var status: PlayerState
 var jump_count = 0
+
 @export var max_jump_count = 2
 
 func move(speed: float, delta):
@@ -35,33 +42,52 @@ func move(speed: float, delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, (deceleration - speed) * delta)
 		
-		
 func _ready() -> void:
 	go_to_idle_state()
 
 
 func _physics_process(delta: float) -> void:
-	print(jump_count)
+	if sliding:
+		sliding_speed = move_toward(sliding_speed , 0, 100 * delta)
+		slide_cooldown = move_toward(slide_cooldown, 5, SLIDE_COOLDOWN_FACTOR * delta)
+	if !sliding:
+		slide_cooldown = move_toward(slide_cooldown, 0, 1 * delta)
+	
+	print(slide_cooldown)
+
+
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	
 	match status:
 		PlayerState.idle:
-			idle_state(delta)
+			idle_state()
 		PlayerState.walk:
-			walk_state(delta)
+			walk_state()
 		PlayerState.jump:
-			jump_state(delta)
+			jump_state()
 		PlayerState.duck:
-			duck_state(delta)
+			duck_state()
 		PlayerState.walk_duck:
-			walk_duck_state(delta)
+			walk_duck_state()
 		PlayerState.fall:
-			fall_state(delta)
+			fall_state()
 		PlayerState.run:
-			run_state(delta)
+			run_state()
+		PlayerState.slide:
+			slide_state()
+	
+	move(current_speed , delta)
 	
 	move_and_slide()
+
+func go_to_slide_state():
+	status = PlayerState.slide
+	sliding_speed = current_speed - slide_speed_bonus
+	current_speed = current_speed + 25
+	sliding = true
+	animacao.play("slide")
+	set_duck_collsion()
+
 
 
 func go_to_run_state():
@@ -98,9 +124,29 @@ func go_to_duck_state():
 	animacao.play("duck")
 	set_duck_collsion()
 	
-func run_state(delta):
+func can_slide():
+	return slide_cooldown == 0	
+
+func slide_state():
+
+	if sliding_speed <= 0 or Input.is_action_just_released("slide"):
+		exit_from_duck_state()
+		if direction != 0:
+			if Input.is_action_pressed("run"):
+				go_to_run_state()
+			else:
+				go_to_walk_state()
+		else:
+			go_to_idle_state()
+		return
+		
+	if Input.is_action_just_pressed("jump"):
+		exit_from_duck_state()
+		go_to_jump_state()
+		return
+		
+func run_state():
 	current_speed = RUN_SPEED
-	move(RUN_SPEED, delta)
 
 	if Input.is_action_just_pressed("jump"):
 		go_to_jump_state()
@@ -109,12 +155,28 @@ func run_state(delta):
 	if Input.is_action_just_released("run"):
 		go_to_walk_state()
 		return	
-
-	pass
 	
-func idle_state(delta):
+	if velocity.x == 0:
+		go_to_idle_state()
+		return
+
+	if Input.is_action_pressed("duck"):
+		go_to_walk_duck_state()
+		return
+	
+	if !is_on_floor():
+		jump_count += 1
+		go_to_fall_state()
+		return
+
+	if Input.is_action_just_pressed("slide") && can_slide():
+		slide_speed_bonus = 45
+		go_to_slide_state()
+		return
+
+
+func idle_state():
 	current_speed = WALK_SPEED
-	move(WALK_SPEED, delta)
 	if velocity.x != 0:
 		go_to_walk_state()
 		return
@@ -127,8 +189,7 @@ func idle_state(delta):
 		go_to_duck_state()
 		return
 
-func fall_state(delta):
-	move(current_speed, delta)
+func fall_state():
 	
 	if Input.is_action_just_pressed("jump") && can_jump():
 		go_to_jump_state()
@@ -145,9 +206,8 @@ func fall_state(delta):
 func can_jump() -> bool:
 	return jump_count < max_jump_count
 
-func walk_state(delta):
+func walk_state():
 	current_speed = WALK_SPEED
-	move(WALK_SPEED, delta)
 	
 	if velocity.x == 0:
 		go_to_idle_state()
@@ -169,9 +229,13 @@ func walk_state(delta):
 	if Input.is_action_pressed("run"):
 		go_to_run_state()
 		return
+		
+	if Input.is_action_just_pressed("slide") && can_slide():
+		slide_speed_bonus = 10
+		go_to_slide_state()
+		return
 	
-func jump_state(delta):
-	move(current_speed, delta)
+func jump_state():
 	if Input.is_action_just_pressed("jump") && can_jump():
 		go_to_jump_state()
 		return
@@ -181,9 +245,8 @@ func jump_state(delta):
 		return
 		
 
-func duck_state(delta):
+func duck_state():
 	current_speed = DUCK_SPEED
-	move(DUCK_SPEED, delta)
 	
 	if Input.is_action_just_released("duck"):
 		exit_from_duck_state()
@@ -194,10 +257,10 @@ func duck_state(delta):
 		go_to_walk_duck_state()
 		return
 
+	
 
-func walk_duck_state(delta):
+func walk_duck_state():
 	current_speed = DUCK_SPEED
-	move(DUCK_SPEED, delta)
 	if Input.is_action_just_released("duck"):
 		exit_from_duck_state()
 		go_to_walk_state()
@@ -213,11 +276,19 @@ func walk_duck_state(delta):
 		go_to_jump_state()
 		return
 
+	if !is_on_floor():
+		jump_count += 1
+		go_to_fall_state()
+		return
+	
+
+
 func set_duck_collsion():
 	colisao.shape.size.y = 11
 	colisao.position.y = 2.5
 	
 func exit_from_duck_state():
+	sliding = false
 	colisao.shape.size.y = 15
 	colisao.position.y = 0.5
 
@@ -228,97 +299,3 @@ func update_direction():
 		animacao.flip_h = true
 	elif direction > 0:
 		animacao.flip_h = false
-
-
-#var walk_speed = 80.0
-#var running_speed = 150.0
-#const SLIDE_BONUS_DISTANCE = 35
-#var sliding_speed
-#var slide_cooldown = 0
-#var JUMP_VELOCITY = -250.0
-#var sliding = false
-#var jumping = false
-#var running = false
-#
-#func slide(ativo: bool, speed: float):
-	#if ativo:
-		#sliding = true
-		#sliding_speed = speed + SLIDE_BONUS_DISTANCE
-		#colisao.shape.size.y = 10
-		#colisao.position.y = 2
-	#else:
-		#colisao.shape.size.y = 15
-		#colisao.position.y = -0.5
-		#sliding_speed = SLIDE_BONUS_DISTANCE
-		#sliding = false
-
-
-#func temp(delta: float) -> void:
-	#if sliding:
-		#sliding_speed = move_toward(sliding_speed, 0, 100 * delta)
-		#slide_cooldown = move_toward(slide_cooldown, 5, 1 * delta)
-		#
-	#if !sliding:
-		#slide_cooldown = move_toward(slide_cooldown, 0, 1 * delta)
-	#
-	#print(slide_cooldown)
-	## Add the gravity.
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
-	## Handle jump.
-	#if Input.is_action_just_pressed("jump") and is_on_floor() and not sliding:
-		#velocity.y = JUMP_VELOCITY
-		#jumping = true
-	#elif is_on_floor():
-		#jumping = false
-#
-	#var direction := Input.get_axis("left", "right")
-	#running = direction && Input.is_action_pressed("run")
-	#
-	## Get the input direction and handle the movement/deceleration.
-	## As good practice, you should replace UI actions with custom gameplay actions.
-	#var speed = sliding_speed if sliding else(running_speed if running else walk_speed)
-	#if direction:
-		#velocity.x = direction * speed
-	#
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, walk_speed)
-	#
-	##Comentario de teste
-	#if sliding and is_on_floor():
-		#if animacao.animation != "slide" and animacao.animation != "slide_loop":
-			#animacao.play("slide")
-		#elif animacao.animation == "slide" and animacao.frame == 2:
-			#animacao.play("slide_loop")
-#
-	#elif not is_on_floor():
-		#animacao.play("jump")
-#
-	#else:
-		#if direction > 0:
-			#animacao.flip_h = false
-			#if running:
-				#animacao.play("running")
-			#else:
-				#animacao.play("walk")
-#
-		#elif direction < 0:
-			#animacao.flip_h = true
-			#if running:
-				#animacao.play("running")
-			#else:
-				#animacao.play("walk")
-#
-		#else:
-			#animacao.play("idle")
-	#
-	#if (direction != 0) and Input.is_action_just_pressed("slide") and is_on_floor() and not jumping:
-		#if slide_cooldown == 0:
-			#if running:
-				#slide(true, running_speed)
-			#else:
-				#slide(true, walk_speed)
-	#elif direction == 0 or Input.is_action_just_released("slide") or jumping or sliding_speed == 0:
-		#slide(false, 0)
-	#
-	#move_and_slide()
